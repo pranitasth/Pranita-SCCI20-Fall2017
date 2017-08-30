b8ce6498546d4d562191a7a63f1ff00f2c08d0e0
Pranita Shrestha <pshrestha001@student.butte.edu>
2017-08-30 20:41:48 +0000

    lab1.3 submission

---
+++ lab11/lab1.2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()  //main function
+{
+    cout << "yyyyyyyy" << endl;
+    cout << "*******" << endl;
+    cout <<"//////" << endl;
+    cout <<"-----" <<endl;
+    cout <<"###" <<endl;
+   
+
+}
+  
---
+++ lecture1.3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int one = 1;
+    int two = 2;
+    int three = 3;
+    int answer = 0;
+    
+    answer = one+one;
+    cout<<answer<<endl;
+    
+    answer = three - one;
+    cout<<answer<<endl;
+    
+    answer = two*three;
+    cout<<answer<<endl;
+    
+    answer = three/two;
+    cout<<answer<<endl;
+}
