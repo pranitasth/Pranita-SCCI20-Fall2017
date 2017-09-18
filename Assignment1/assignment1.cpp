568ed76818d268091eef9b29948d8dbf36bd69db
Pranita Shrestha <pshrestha001@student.butte.edu>
2017-09-18 19:31:00 +0000

    Assignment1.submission

---
+++ Assignment1/assignment1.cpp
@@ -0,0 +1,40 @@
+//Created by : Pranita Shrestha
+//Created on : 9/18/2017
+
+// This lab is about calculating the acceleration with the help of velocity, time, and distance.
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+struct PlaneInfo {
+
+double distance;
+double speed;
+double time;
+double acceleration;
+double meters;
+};
+
+int main()
+{
+PlaneInfo jet_fighter;
+
+cout << " Enter jets takeoff speed in Km/hr.: ";
+cin >> jet_fighter.speed;
+cout << "Enter distance of takeoff in meters.: ";
+cin >> jet_fighter.meters;
+
+jet_fighter.time = (7.2*(jet_fighter.distance)/ jet_fighter.speed);
+jet_fighter.acceleration = (jet_fighter.speed / (jet_fighter.meters * 3.6));
+
+cout << "The acceleration of a jet";
+cout << "traveling at " << jet_fighter.speed <<"Km/Hr" << endl;
+cout << "at a distance of " << jet_fighter.distance << " meters" <<  endl;
+cout << "Is " << jet_fighter.acceleration << " meters/seconds^2" << endl;
+cout << ", with a time of " << time << " seconds to reach takeoff speed" << endl;
+
+
+}
+    
