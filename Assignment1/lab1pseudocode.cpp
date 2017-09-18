48646062135a4bdcb27e0f9d7eca6d8ed60dbddd
Pranita Shrestha <pshrestha001@student.butte.edu>
2017-09-18 19:28:41 +0000

    Assignment1.submission

---
+++ Assignment1/lab1pseudocode.cpp
@@ -0,0 +1,24 @@
+//Created by : Pranita Shrestha
+//Created on : 9/18/2017
+
+// This lab is about calculating the acceleration with the help of velocity, time, and distance.
+
+
+//inputs
+Create a struct for acceleration. The objet is a jet fighter. It's attributes are distance, time, speed, aceleration,and meters.
+Have an input for distance, speed, time
+Have a speed of jet fighter in km/hr
+Have a distance of takeoff speed in m/s^2
+
+
+//conversions
+the acceleration should be in meter/s^2
+the distance should be in km/hr.
+
+//calculations
+time = (7.2*(distance)/ speed);
+acceleration = (speed /(meters * 3.6))
+
+//outputs
+It will show  the acceleration of  a jet fighter launched from an aircraft-carrier catapult at km/hr.
+It will show the distance of the jet’s takeoff speed at m/s^2.
